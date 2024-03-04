61229975d8bd05a98a6f1c92416bd98e32ee041e
Ali Mroweh <amroweh@gmail.com>
2024-03-04 22:04:27 +0200

    working on customized nodes

--- graph.js
+++ graph.js
@@ -1,19 +1,16 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm'
 import {dragstarted, dragged, dragended} from '../utils/dragFunctions.js'
 import settings from './settings.js'
+import {getNodeDimensions, getTextPosition} from './utils/shapeFunctions.js'
 
 const Graph = ({nodes, links}) => {
 	// set the dimensions and margins of the graph
 	const margin = {top: 0, right: 0, bottom: 0, left: 0},
 		width = settings.GRAPH_WIDTH - margin.left - margin.right,
 		height = settings.GRAPH_HEIGHT - margin.top - margin.bottom - settings.GRAPH_BORDER_SIZE,
-		radius = settings.NODE_RADIUS_BRANCH,
+		radius = settings.NODE_RADIUS_COMMIT,
 		textOffsetX = 1.2 * radius,
-		textOffsetY = 0,
-		labelTextOffsetX = -radius - 50,
-		labelTextOffsetY = -radius,
-		labelRectOffsetX = labelTextOffsetX - 5.5,
-		labelRectOffsetY = labelTextOffsetY - 17
+		textOffsetY = 0
 
 	// append the svg object to the Box component with the dimensions defined above (removes if already there)
 	if (!d3.select('#graph').select('svg').empty()) {
@@ -47,7 +44,7 @@ const Graph = ({nodes, links}) => {
 		.append('defs')
 		.append('marker')
 		.attr('id', 'arrowhead_branch_branch')
-		.attr('refX', settings.NODE_RADIUS_BRANCH + 10) // Add marker width to radius
+		.attr('refX', settings.NODE_WIDTH_BRANCH + 10) // Add marker width to radius
 		.attr('refY', 3.5)
 		.attr('orient', 'auto')
 		.attr('markerWidth', 10)
@@ -86,15 +83,12 @@ const Graph = ({nodes, links}) => {
 
 	// Initialize the nodes
 	const node = svg
-		.selectAll('circle')
+		.selectAll('rect')
 		.data(nodes)
 		.enter()
-		.append('circle')
-		.attr('r', d => {
-			if (d.type === 'commit' || d.type === 'mergecommit') return settings.NODE_RADIUS_COMMIT
-			else if (d.type === 'branch') return settings.NODE_RADIUS_BRANCH
-			else if (d.type === 'head') return settings.NODE_RADIUS_HEAD
-		})
+		.append('rect')
+		.attr('width', d => getNodeDimensions(d.type).width)
+		.attr('height', d => getNodeDimensions(d.type).height)
 		.style('fill', d => {
 			if (d.type === 'commit' || d.type === 'mergecommit') return settings.NODE_COLOR_COMMIT
 			else if (d.type === 'branch') return settings.NODE_COLOR_BRANCH
@@ -105,6 +99,14 @@ const Graph = ({nodes, links}) => {
 			if (d.type === 'head') return settings.NODE_BORDER_COLOR_HEAD
 		})
 		.style('stroke-width', '3px')
+		.style('rx', d => (d.type === 'commit' || d.type === 'mergecommit') && 50)
+		.style('ry', d => (d.type === 'commit' || d.type === 'mergecommit') && 30)
+		.attr('transform', d => {
+			if (d.type === 'commit' || d.type === 'mergecommit')
+				return 'translate(-' + settings.NODE_RADIUS_COMMIT / 2 + ' -' + settings.NODE_RADIUS_COMMIT / 2 + ')'
+			else if (d.type === 'branch' || d.type === 'head')
+				return 'translate(-' + settings.NODE_WIDTH_BRANCH / 2 + ' -' + settings.NODE_HEIGHT_BRANCH / 2 + ')'
+		})
 		.call(
 			d3
 				.drag()
@@ -120,11 +122,14 @@ const Graph = ({nodes, links}) => {
 	const branchNameText = branchContainer
 		.append('text')
 		.attr('class', 'branchNameText')
-		.text(d => d.name) // adds asterisk to head branch
-		.attr('fill', d => d.type === 'head' && settings.NODE_BORDER_COLOR_HEAD)
-		.attr('transform', function (d) {
-			if (d.type === 'head') return 'translate(-' + this.getBBox().width / 2 + ' +' + this.getBBox().height / 4 + ')'
+		.text(d => {
+			if (d.name.length > settings.BRANCH_LABEL_MAX_LENGTH) {
+				if (d.type === 'branch' || d.type === 'head') return d.name.slice(0, settings.BRANCH_LABEL_MAX_LENGTH) + '..'
+				if (d.type === 'commit' || d.type === 'head') return d.name.slice(0, settings.COMMIT_LABEL_MAX_LENGTH) + '..'
+			}
+			return d.name
 		})
+		.attr('fill', d => d.type === 'head' && settings.NODE_BORDER_COLOR_HEAD)
 
 	// This function is run at each iteration of the force algorithm, updating the nodes position.
 	// note: d is provided by the simulation
@@ -135,18 +140,15 @@ const Graph = ({nodes, links}) => {
 			.attr('x2', d => d.target.x)
 			.attr('y2', d => d.target.y)
 
-		node.attr('cx', d => d.x).attr('cy', d => d.y)
+		node.attr('x', d => d.x).attr('y', d => d.y)
 		branchNameText
-			.attr('x', d => {
-				if (d.type === 'head') return d.x
-				return d.x + textOffsetX
+			.attr('x', function (d) {
+				if (d.type === 'commit' || d.type === 'mergecommit') return d.x + settings.NODE_RADIUS_COMMIT * 0.7
+				else if (d.type === 'branch' || d.type === 'head') return d.x - this.getBBox().width / 2
 			})
-			.attr('y', d => {
-				if (d.type === 'head') return d.y
-				return d.y + textOffsetY
+			.attr('y', function (d) {
+				return d.y + this.getBBox().height / 4
 			})
-		// labelText.attr('x', d => d.x + labelTextOffsetX).attr('y', d => d.y + labelTextOffsetY)
-		// labelBox.attr('x', d => d.x + labelRectOffsetX).attr('y', d => d.y + labelRectOffsetY)
 	}
 }
 
--- utils/shapeFunctions.js
+++ utils/shapeFunctions.js
@@ -1,3 +1,5 @@
+import settings from '../settings.js'
+
 export const createNode_Rect = (type, width, height, fill, stroke, strokeWidth) => {
 	const rect = document.createElement('rect')
 	rect.setAttribute('width', width)
@@ -17,4 +19,30 @@ export const createNode_Circle = (type, radius, fill, stroke, strokeWidth) => {
 	circle.style.strokeWidth = strokeWidth
 	circle.classList.add(type)
 	return circle
+}
+
+export const getNodeDimensions = type => {
+	if (type === 'branch' || type === 'head')
+		return {
+			width: settings.NODE_WIDTH_BRANCH,
+			height: settings.NODE_HEIGHT_BRANCH
+		}
+	if (type === 'commit' || type === 'mergecommit')
+		return {
+			width: settings.NODE_RADIUS_COMMIT,
+			height: settings.NODE_RADIUS_COMMIT
+		}
+}
+
+export const getTextPosition = type => {
+	if (type === 'branch' || type === 'head')
+		return {
+			x: 0,
+			y: 0
+		}
+	if (type === 'commit' || type === 'mergecommit')
+		return {
+			x: settings.NODE_RADIUS_COMMIT,
+			y: settings.NODE_RADIUS_COMMIT
+		}
 }
