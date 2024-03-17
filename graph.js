3fcc8bf61bb608420119ade5a8eb4fd34b678045
Ali Mroweh <amroweh@gmail.com>
2024-03-17 22:59:26 +0000

    fixed import for dragFunctions

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm'
-import {dragstarted, dragged, dragended} from '../utils/dragFunctions.js'
+import {dragstarted, dragged, dragended} from './utils/dragFunctions.js'
 import settings from './settings.js'
 import {
 	createSvgArrowHead,
