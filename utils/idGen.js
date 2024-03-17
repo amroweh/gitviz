a6b03380a96693007b1a7b9c2315db45a1024a71
Ali Mroweh <amroweh@gmail.com>
2024-03-17 22:37:03 +0000

    some refactoring

--- utils/idGen.js
+++ utils/idGen.js
@@ -1,30 +1,18 @@
-function generateUniqueString() {
-	// Get the current timestamp in milliseconds
+const generateUniqueString = () => {
 	const timestamp = new Date().getTime()
-
-	// Generate a random 16-byte string
-	const randomString = Array.from(
-		{length: 16},
-		() => Math.floor(Math.random() * 256).toString(16)
-		.padStart(2, '0')
+	const randomString = Array.from({length: 16}, () =>
+		Math.floor(Math.random() * 256)
+			.toString(16)
+			.padStart(2, '0')
 	).join('')
-
-	// Combine timestamp and randomString to create a unique string
-	const uniqueString = randomString + timestamp.toString(16)
-
-	return uniqueString
+	return randomString + timestamp.toString(16)
 }
 
-let commitIds = 1,
-	treeIds = 0,
+let treeIds = 0,
 	blobIds = 0
 
 export const newCommitId = () => {
 	return generateUniqueString()
-	// commitIds++
-	// console.log(commitIds)
-	// console.log(sha1(Math.random(20)))
-	// return '#' + sha1(commitIds)
 }
 export const newTreeId = () => treeIds++
 export const newBlobId = () => blobIds++
